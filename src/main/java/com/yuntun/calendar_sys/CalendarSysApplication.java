e1becc8ca5235c766160f4ca325fd57a1845bcb3
whj <1475351569@qq.com>
2020-11-04 18:20:29 +0800

    [add]初始化

---
+++ src/main/java/com/yuntun/calendar_sys/CalendarSysApplication.java
@@ -0,0 +1,13 @@
+package com.yuntun.calendar_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuntun/calendar_sys/CalendarSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuntun.calendar_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
